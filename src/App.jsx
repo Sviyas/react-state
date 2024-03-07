31e0759c0eabdcfa701dc5b2651d47267cdc4a0d
Sviyas <viyas2001@gmail.com>
2024-03-07 22:23:59 +0530

    add Close Button on header page add event listener for manageing their states

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import './App.css';
 
-const textMessages = ['Learn React 😍', 'Apply Jobs 🤗  ', 'Invest your new income 🤗'];
+const textMessages = ['Welcome to React 😍', 'Learn React 🤗  ', 'Invest Your TIme get Some Knowledge 🤗'];
 /**
  * @returns Form Submitions
  */
 function App() {
   const [count, setCount] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   const handlPrevious = () => {
     if (count > 1) setCount(count - 1);
@@ -17,22 +18,32 @@ function App() {
   };
 
   return (
-    <div className='steps'>
-      <div className='numbers'>
-        <div className={count >= 1 ? 'active' : ''}>1</div>
-        <div className={count >= 2 ? 'active' : ''}>2</div>
-        <div className={count >= 3 ? 'active' : ''}>3</div>
-      </div>
+    <>
+      <h1>React State</h1>
 
-      <p className='message'>
-        Step : {count} : {textMessages[count - 1]}
-      </p>
+      <button className='close' onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
 
-      <div className='buttons'>
-        <button onClick={() => handlPrevious()}>Preview</button>
-        <button onClick={() => handleNext()}>Next</button>
-      </div>
-    </div>
+      {isOpen && (
+        <div className='steps'>
+          <div className='numbers'>
+            <div className={count >= 1 ? 'active' : ''}>1</div>
+            <div className={count >= 2 ? 'active' : ''}>2</div>
+            <div className={count >= 3 ? 'active' : ''}>3</div>
+          </div>
+
+          <p className='message'>
+            Step : {count} : {textMessages[count - 1]}
+          </p>
+
+          <div className='buttons'>
+            <button onClick={() => handlPrevious()}>Preview</button>
+            <button onClick={() => handleNext()}>Next</button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
